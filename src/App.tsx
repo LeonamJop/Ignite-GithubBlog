3497c544e13e0d41628448888e41b562683e4c08
Leonam Silva <leonam.murilo@gmail.com>
2024-08-05 17:12:07 -0300

    Style: create a global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyled } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
       <GlobalStyled />
-
-      <h1>Hello World!!</h1>
     </ThemeProvider>
   )
 }
