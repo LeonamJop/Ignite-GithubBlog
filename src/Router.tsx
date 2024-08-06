a1ed2d1d2c13e5ea3cac52e9aaf3b3fcf22d4fb8
Leonam Silva <leonam.murilo@gmail.com>
2024-08-06 11:55:45 -0300

    feat: create profile card

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayouts' 
+import { CardProfile } from './components/CardProfile'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="*" element={<h1>Teste</h1>} />
-        <Route path="/" element={<h1>Teste</h1>} />
+        <Route path="/" element={<CardProfile/>} />
       </Route>
     </Routes>
   )
