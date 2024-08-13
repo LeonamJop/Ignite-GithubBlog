2ed86c11deb7d0f83a9b7a339f3c5907d0a50a0e
Leonam Silva <leonam.murilo@gmail.com>
2024-08-13 11:47:16 -0300

    style: refactor card infos

--- src/components/Home/CardProfile/index.tsx
+++ src/components/Home/CardProfile/index.tsx
@@ -1,39 +1,40 @@
 import { ProfileBio, ProfileContainer, ProfileInfo, ProfilePhoto } from "./styles";
-import { user } from '../../../services/api'
+import { user } from '../../../services/api';
 
 import { useEffect, useState } from "react";
 
-import gitimg from '../../../assets/icons/github-brands-solid.svg'
-import building from '../../../assets/icons/building-solid.svg'
-import group from '../../../assets/icons/user-group-solid.svg'
+import gitimg from '../../../assets/icons/github-brands-solid.svg';
+import building from '../../../assets/icons/building-solid.svg';
+import group from '../../../assets/icons/user-group-solid.svg';
+import link from '../../../assets/icons/arrow-up-right-from-square-solid.svg';
 
 export function CardProfile() {
-    const [userName, setUserName] = useState('')
-    const [bio, setBio] = useState('')
-    const [login, setLogin] = useState('')
-    const [followers, setFollowers] = useState('')
-    const [company, setCompany] = useState('')
-    const [url, setUrl] = useState('')
-    const [avatar, setAvatar] = useState('')
+    const [userName, setUserName] = useState('');
+    const [bio, setBio] = useState('');
+    const [login, setLogin] = useState('');
+    const [followers, setFollowers] = useState('');
+    const [company, setCompany] = useState('');
+    const [url, setUrl] = useState('');
+    const [avatar, setAvatar] = useState('');
 
     useEffect(() => {
 
         const fetchData = async () => {
-            const response = await user
+            const response = await user;
 
             if (response.data.erro) return;
 
-            setUserName(response.data.name)
-            setBio(response.data.bio)
-            setLogin(response.data.login)
-            setFollowers(response.data.followers)
-            setCompany(response.data.company)
-            setUrl(response.data.html_url)
-            setAvatar(response.data.avatar_url)
+            setUserName(response.data.name);
+            setBio(response.data.bio);
+            setLogin(response.data.login);
+            setFollowers(response.data.followers);
+            setCompany(response.data.company);
+            setUrl(response.data.html_url);
+            setAvatar(response.data.avatar_url);
         }
 
-        fetchData()
-    },[user])
+        fetchData();
+    },[user]);
 
     return (
         <ProfileContainer>
@@ -44,8 +45,11 @@ export function CardProfile() {
                 <span>{bio}</span>
                 <ProfileInfo>
                     <div>
-                        <img src={gitimg} alt="" />
-                        <a href={url} target="_blank">{login}</a>
+                        <a href={url} target="_blank" title="Acessar GITHUB">
+                            <img src={gitimg} alt="" />
+                            {login}
+                            <img src={link} alt="" />
+                        </a>
                     </div>
 
                     <div>
